0492e00431f667182b4276389d3d2f34fb5d29ff
Alvaro Molina <alvmol706@alu.centroafuera.es>
2021-10-14 12:46:41 +0200

    PASOS

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    /*
+        crear variable de la palabra que buscamos (lista)
+        saber el tamaño de la palabra y escribir tantos _ como letras tenga la palabra
+        bucle para preguntar al usuario que letra quiere introducir,
+        al escribir la letra comprobar si la palabra secreta contiene la letra y cambiar la _ por la letra (recorrer la palabra secreta y comprobar si la letra esta dentro de la palabra secreta)
+        hacer contador y sumarle +1 cada vez que se introduzca una letra por teclado
+        guardar las letras introducidas en una variable y cada vez que se escriba una nueva comprobar si ya a escrito la letra
+        terminar cuando la palabra secreta no tiene mas _
+        imprimir el contador
+     */
+}
